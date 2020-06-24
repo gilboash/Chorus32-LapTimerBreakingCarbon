realhuno/Chorus32-ESP32LapTimer

Move skipfirstlap variable and check it when calculating the best lap.

ESP32LapTimer/Laptime.cpp
@@ -13,6 +13,7 @@ static uint8_t best_lap_num[MAX_NUM_PILOTS];
 
 static uint32_t MinLapTime = 5000;  //this is in millis
 static uint32_t start_time = 0;
+static uint8_t skip_first_lap = 0; // 0 means start table is before the laptimer, so first lap is not a full-fledged lap (i.e. don't respect min-lap-time for the very first lap)
 
 void resetLaptimes() {
 	memset(LapTimes, 0, MAX_NUM_PILOTS * MAX_LAPS_NUM * sizeof(LapTimes[0][0]));
@@ -46,8 +47,15 @@ uint32_t getLaptime(uint8_t receiver) {
 uint8_t addLap(uint8_t receiver, uint32_t time) {
 	LapTimePtr[receiver] = LapTimePtr[receiver] + 1;
 	LapTimes[receiver][LapTimePtr[receiver]] = time;
-	if(getLaptimeRel(receiver) > getLaptimeRel(receiver, best_lap_num[receiver])) {
-		best_lap_num[receiver] = LapTimePtr[receiver];
+	Serial.print("New lap: ");
+	Serial.print(getLaptimeRel(receiver));
+	Serial.print(" Best: ");
+	Serial.println(getLaptimeRel(receiver, best_lap_num[receiver]));
+	if((getLaptimeRel(receiver) > getLaptimeRel(receiver, best_lap_num[receiver]) || getLaptimeRel(receiver, best_lap_num[receiver]) == 0)) {
+		// skip best time if we skip the first lap
+		if(!(LapTimePtr[receiver] == 1 && skip_first_lap)) {
+			best_lap_num[receiver] = LapTimePtr[receiver];
+		}
 	}
 	return LapTimePtr[receiver];
 }
@@ -70,4 +78,12 @@ uint8_t getCurrentLap(uint8_t receiver) {
 
 void startRaceLap() {
 	start_time = millis();
+}
+
+void setSkipFirstLap(uint8_t shouldWaitForFirstLap) {
+	skip_first_lap = shouldWaitForFirstLap;
+}
+
+uint8_t getSkipFirstLap() {
+	return skip_first_lap;
 }

gilboash/Chorus32-LapTimerBreakingCarbon

ESP32LapTimer/src/Laptime.cpp
@@ -83,8 +83,11 @@ uint32_t getLaptimeRel(uint8_t receiver) {
 uint8_t addLap(uint8_t receiver, uint32_t time) {
   lap_counter[receiver] = lap_counter[receiver] + 1;
   LapTimes[receiver][lap_counter[receiver]] = time;
-  if(getLaptimeRel(receiver) > getLaptimeRel(receiver, best_lap_num[receiver])) {
-    best_lap_num[receiver] = lap_counter[receiver];
+  if((getLaptimeRel(receiver) > getLaptimeRel(receiver, best_lap_num[receiver]) || getLaptimeRel(receiver, best_lap_num[receiver]) == 0)) {
+    // skip best time if we skip the first lap
+    if(!(lap_counter[receiver] == 1 && skip_first_lap)) {
+      best_lap_num[receiver] = lap_counter[receiver];
+    }
   }
   return lap_counter[receiver];
 }