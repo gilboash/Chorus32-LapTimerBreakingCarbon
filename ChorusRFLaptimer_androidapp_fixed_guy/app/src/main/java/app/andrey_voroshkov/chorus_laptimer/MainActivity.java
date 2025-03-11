1a22a2317eb826c5515d58a3c463954fc04ae03c
gilboash <gilboash@gmail.com>
2025-03-11 11:51:17 +0200

    add permission ask

--- ChorusRFLaptimer_androidapp_fixed_guy/app/src/main/java/app/andrey_voroshkov/chorus_laptimer/MainActivity.java
+++ ChorusRFLaptimer_androidapp_fixed_guy/app/src/main/java/app/andrey_voroshkov/chorus_laptimer/MainActivity.java
@@ -393,6 +393,12 @@ public class MainActivity extends AppCompatActivity implements ConnectionListene
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     REQUEST_WRITE_STORAGE_CODE);
         }
+
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+
     }
 
     private  void initBroadcastReceiverForBluetooth() {
